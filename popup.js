681133aa8beb55cd069d59095474b424354917e2
irene-portillo <irenemaideli@gmail.com>
2025-04-21 01:48:47 -0400

    Got Gemni to work ! -- mostly with base code but had a bunch of trouble getting it to load in

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log("This is a ppopup! wait isnt this js ??")
+console.log("Please please PLEASEEEE EELOOADDDDDD");
+
+import {GoogleGenAI} from './node_modules/@google/genai/dist/web/index.mjs';
+// const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const ai = new GoogleGenAI({apiKey: 'AIzaSyD4ep-RLPf8Ttmc1RNAC7A2n76XkWSCOfY'});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash-001',
+    contents: 'Why is the sky blue?',
+  });
+  console.log(response.text);
+}
+
+main();
